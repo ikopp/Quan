2eb114fd55b11a5d55702ac003f4779284763ed6
ikopp <55929726+ikopp@users.noreply.github.com>
2025-04-24 12:48:00 +0400

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-from pydub import AudioSegment
-
-audio = AudioSegment.silent(duration=1000)  # 1 second of silence
-audio.export("test_output.mp3", format="mp3")
-print("Test file created: test_output.mp3")
